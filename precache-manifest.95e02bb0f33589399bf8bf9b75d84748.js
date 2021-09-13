536ee131faf7852fe9d139328ab9e7952d133d15
Madelin Capellan <madelincapellan23@gmail.com>
2021-09-12 22:30:40 -0400

    Updates

---
+++ precache-manifest.95e02bb0f33589399bf8bf9b75d84748.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d91f5b2d2e3491127679b58baa68d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e065d34f46036b1b73c9",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "5e51efda24f19cbbcbd9",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "38b6e3f97d3180c5a0de",
+    "url": "/static/css/main.509cd038.chunk.css"
+  },
+  {
+    "revision": "7071f35fa32d1e4a36e5",
+    "url": "/static/js/2.729ada1c.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.729ada1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e065d34f46036b1b73c9",
+    "url": "/static/js/3.5984b742.chunk.js"
+  },
+  {
+    "revision": "5e51efda24f19cbbcbd9",
+    "url": "/static/js/4.a616ff42.chunk.js"
+  },
+  {
+    "revision": "38b6e3f97d3180c5a0de",
+    "url": "/static/js/main.65de4583.chunk.js"
+  },
+  {
+    "revision": "aa2d0bed346ec82819ad",
+    "url": "/static/js/runtime-main.ebbf1225.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7110cb321878a3236109ceb47d91e118",
+    "url": "/static/media/BPD-Logo.7110cb32.jpg"
+  },
+  {
+    "revision": "46c7c7c8c9468ffd4958b6a4ce1b3ead",
+    "url": "/static/media/CSFPC-CertiProf-Badge.46c7c7c8.webp"
+  },
+  {
+    "revision": "8c26a314ae321c8bbc13c8a91da75c47",
+    "url": "/static/media/CTC-Logo.8c26a314.png"
+  },
+  {
+    "revision": "f0034c65ffff38bc5af323e3b2bddca8",
+    "url": "/static/media/Dominican-Republic-Flag.f0034c65.png"
+  },
+  {
+    "revision": "a7d96bd5d2a8812abe23ab15da7632be",
+    "url": "/static/media/Kuepa-Logo.a7d96bd5.webp"
+  },
+  {
+    "revision": "7e8403f7ce2f524cad16e2b9a2135d32",
+    "url": "/static/media/LinkedIn-Learning-Logo.7e8403f7.png"
+  },
+  {
+    "revision": "346e33b76f693e80f58cccff9a69e423",
+    "url": "/static/media/MESCYT-Logo.346e33b7.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "dbab12e683e1615bac4235c59cbb82dc",
+    "url": "/static/media/O&M-Logo.dbab12e6.jpg"
+  },
+  {
+    "revision": "1d94f142db2bf98b880574cd8e579804",
+    "url": "/static/media/Republica-Digital-Logo.1d94f142.jpg"
+  },
+  {
+    "revision": "0c791fcc7d93d7b441615845fb054e31",
+    "url": "/static/media/School-Logo.0c791fcc.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
